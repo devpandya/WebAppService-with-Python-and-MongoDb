e4566c0165bf31cea6872206fc72c8fdf891e069
dpandya <dpandya@vitahealthsoftware.com>
2020-11-03 16:05:09 +0530

    Added Features to MasterData.

--- master.py
+++ master.py
@@ -12,6 +12,7 @@ master_api = Blueprint('master_api', __name__)
 @master_api.route('/getmaster', methods=['GET'])
 def get_master_data():
      Resources = mongo.db.Resources.find({},{'_id':False} )
-     master_data=jsonify({"Resources": list(Resources)})
+     Features = mongo.db.Features.find({"isactive":True},{'_id':False} )
+     master_data=jsonify({"Resources": list(Resources), "Features":list(Features)})
      master_data.status_code = 200
      return master_data
